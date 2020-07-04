f715b8fcd370761f0e27d53492393ac6e57db888
vdonoladev <victordonolaf@gmail.com>
2020-07-04 19:37:09 -0300

    Iniciando a biblioteca gráfica embutida do Python (built-in) tkinter

---
+++ gui-calculadora.py
@@ -0,0 +1,7 @@
+from tkinter import *  # Importando tudo da biblioteca
+
+root = Tk()  # Criando nossa janela, uma instância de Tk()
+root.title('Minha GUI')  # Nomeando a janela
+
+root.mainloop()  # Criando um loop eterno (basicamente um "while True" por debaixo dos 
+                 # panos para abrir nossa janela)
