c30355607396b17b0958ea9bf95fa5725842a1a4
vdonoladev <victordonolaf@gmail.com>
2020-07-04 19:44:52 -0300

    Começando a criar o código

--- gui-calculadora.py
+++ gui-calculadora.py
@@ -1,7 +1,10 @@
 from tkinter import *  # Importando tudo da biblioteca
 
 root = Tk()  # Criando nossa janela, uma instância de Tk()
-root.title('Minha GUI')  # Nomeando a janela
+root.title('Calculadora')  # Nomeando a janela
+
+window_title = Label(root, text='Calculadora', font=('Helvetica',
+	18)).grid(row=0, column=0, columnspan=2)  # Título para a calculadora
 
 root.mainloop()  # Criando um loop eterno (basicamente um "while True" por debaixo dos 
                  # panos para abrir nossa janela)
