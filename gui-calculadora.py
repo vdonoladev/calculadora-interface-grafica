bc07d8b3df54d7f3271f302ff22fc91e98b5af1c
vdonoladev <victordonolaf@gmail.com>
2020-07-04 20:21:13 -0300

    Código completo

--- gui-calculadora.py
+++ gui-calculadora.py
@@ -1,31 +1,31 @@
-from tkinter import *  
+from tkinter import *
 
 def calculates():
-	which_button = selected_operation.get()
-	value_1 = float(entry_field_1.get())
-	value_2 = float(entry_field_2.get())
-	if which_button == 1:
-		resultado = value_1 + value_2
-	elif which_button == 2:
-		resultado = value_1 - value_2
-	elif which_button == 3:
-		resultado = value_1 * value_2
-	elif which_button == 4:
-		resultado = value_1 / value_2
+    which_button = selected_operation.get()
+    value_1 = float(entry_field_1.get())
+    value_2 = float(entry_field_2.get())
+    if which_button == 1:
+        resultado = value_1 + value_2
+    elif which_button == 2:
+        resultado = value_1 - value_2
+    elif which_button == 3:
+        resultado = value_1 * value_2
+    elif which_button == 4:
+        resultado = value_1 / value_2  
+       
+    result_field.delete(0, END)
+    result_field.insert(0, resultado)
 
-	result_field.delete(0, END)
-	result_field.insert(0, resultado)
+root = Tk()
+root.title('Calculadora rústica')
 
-root = Tk()  
-root.title('Calculadora')  
+window_title = Label(root, text='Calculadora rústica', font=('Helvetica', 18)).grid(row=0, column=0, columnspan=2)
 
-window_title = Label(root, text='Calculadora', font=('Helvetica', 18)).grid(row=0, column=0, columnspan=2)  
-
-label_1 = Label(root, text='Digite um número').grid(row=1, column=0) 
+label_1 = Label(root, text='Digite um número').grid(row=1, column=0)
 entry_field_1 = Entry(root, text='Número 1', width=10)
 entry_field_1.grid(row=1, column=1)
 
-label_2 = Label(root, text='Digite outro número').grid(row=2, column=0)  
+label_2 = Label(root, text='Digite outro número').grid(row=2, column=0)
 entry_field_2 = Entry(root, text='Número 2', width=10)
 entry_field_2.grid(row=2, column=1)
 
@@ -47,9 +47,9 @@ division_button.grid(row=5, column=1)
 
 botao = Button(root, text='Calcular operação', command=calculates, width='30').grid(row=6, column=0, columnspan=2)
 
-label_3 = Label(root, text='Resultado').grid(row=7, column=0)
+label_4 = Label(root, text='Resultado').grid(row=7, column=0)
 
 result_field = Entry(root)
 result_field.grid(row=7, column=1)
 
-root.mainloop() 
+root.mainloop()
